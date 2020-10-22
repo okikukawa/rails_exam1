afd100047767f8d7cb35aba09441cb78abc6e5e9
yosuke kikukawa <kikukawa0131@gmail.com>
2020-10-22 23:28:52 +0900

    contentをDBに保存する機能を追加

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -4,4 +4,12 @@ class DiariesController < ApplicationController
   def new
     @diary = Diary.new
   end
+  def create
+    Diary.create(diary_params)
+    redirect_to new_diary_path
+  end
+  private
+  def diary_params
+    params.require(:diary).permit(:content)
+  end
 end
